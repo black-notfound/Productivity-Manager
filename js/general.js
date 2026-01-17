2cb85fb8b5c93c7b4798a75a295a52b1e0e0c6a2
404 <zionkeim387@gmail.com>
2026-01-16 23:06:55 -0300

    Adicinando menu hamburguer no mobile

--- js/general.js
+++ js/general.js
@@ -3,3 +3,10 @@ const theme = document.getElementById("theme");
 function changeTheme() {
     document.body.classList.toggle("dark")
 }
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
